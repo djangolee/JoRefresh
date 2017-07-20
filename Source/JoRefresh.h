c57a955e2a6d8b8c0778915800795674598413a7
django <djangolee@sina.com>
2017-07-20 17:23:54 +0800

    init workspace and .gitigonre

---
+++ Source/JoRefresh.h
@@ -0,0 +1,19 @@
+//
+//  JoRefresh.h
+//  JoRefresh
+//
+//  Created by django on 7/20/17.
+//  Copyright © 2017 django. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for JoRefresh.
+FOUNDATION_EXPORT double JoRefreshVersionNumber;
+
+//! Project version string for JoRefresh.
+FOUNDATION_EXPORT const unsigned char JoRefreshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JoRefresh/PublicHeader.h>
+
+
